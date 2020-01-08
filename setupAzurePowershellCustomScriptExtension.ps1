3acc77575cfd27e696b4417222b7a8e8fe7da43a
Matt Lunzer <malunzer@microsoft.com>
2020-01-08 13:37:25 -0600

    updates

--- setupAzurePowershellCustomScriptExtension.ps1
+++ setupAzurePowershellCustomScriptExtension.ps1
@@ -3,12 +3,12 @@
 $rg = <resourceGroupName>
 $vm = <computerName>
 $region = <region>
-$fileUri = 
-$fileName = <filename>
+$fileUri = https://raw.githubusercontent.com/mattlunzer/powerShell/master/
+$fileName = setupAzurePowershellCustomScriptExtension.ps1
 
 Set-AzVMCustomScriptExtension -ResourceGroupName $rg `
     -VMName $vm `
     -Location $region `
     -FileUri $fileUri `
     -Run $fileName `
-    -Name DemoScriptExtension  
+#    -Name DemoScriptExtension  
